a51d13600b163e2ffd17039281793b965716eb39
nodcah <nodcah@gmail.com>
2016-11-20 13:02:43 -0500

    Commented a few of the ideas I had for basic layout

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// javascript.js
+// Desc: all of the javascript to run the "Who's Free?" web app
+// Authoer: Noah Del Coro
+
+
+// ===== VARIABLES =====
+// Array of
+
+
+// ===== CUSTOM CLASSES =====
+// Make custom time class with function to return current time
+
+
+// ===== HELPER FUNCTIONS =====
+// Function to search the webpage and save as local data
+
+// Reloads table with given time (default time: current time)
+
+
+// ===== EVENT LISTENERS =====
+// Listener for button press
+
+
+// ===== MAIN =====
