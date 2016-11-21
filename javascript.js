8da731f67feaf8f921e0feea77bf18c4cf5f97e0
nodcah <nodcah@gmail.com>
2016-11-20 20:42:46 -0500

    Made help html file, Time class, and basic webpage layout (UNTESTED)

--- javascript.js
+++ javascript.js
@@ -10,6 +10,21 @@
 // ===== CUSTOM CLASSES =====
 // Make custom time class with function to return current time
 
+// hour=hour (int), min=minute (int), am=T for AM & F for PM (boolean)
+function Time(hour, min, am) {
+    this.hour = hour;
+    this.min = min;
+    this.am = am;
+
+    function gt(t) {
+        if (this.am && t.pm) return true;
+        else if (this.pm && t.am) return false;
+        else if (this.hour > t.hour) return true;
+        else if (this.hour < t.hour) return false;
+        else return (this.min > t.min);
+    }
+
+}
 
 // ===== HELPER FUNCTIONS =====
 // Function to search the webpage and save as local data
